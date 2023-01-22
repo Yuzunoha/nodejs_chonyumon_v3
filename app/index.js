bebe7ed60d0c252e19db0365d833ecb81b365271
Yuzunoha <studysteadystandby@gmail.com>
2023-01-22 21:53:22 +0900

    読み込んだ

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
-  // response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('<html><h1>Hello World</h1></html>');
+  fs.readFile('./html_files/index.html', 'UTF-8', (err, data) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+  });
 });
 
 server.listen(3000);
