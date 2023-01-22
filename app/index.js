4e470f65bfb5ff162a5c22ff7470a0abf254b790
Yuzunoha <studysteadystandby@gmail.com>
2023-01-22 21:57:43 +0900

    promise

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((request, response) => {
-  fs.readFile('./html_files/index.html', 'UTF-8', (err, data) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
+const server = http.createServer(async (request, response) => {
+  const data = await new Promise((resolve) => {
+    fs.readFile('./html_files/index.html', 'UTF-8', (e, d) => resolve(d));
   });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(data);
+  response.end();
 });
 
 server.listen(3000);
