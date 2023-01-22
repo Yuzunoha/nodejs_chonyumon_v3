eb176afce241d58b88b1d9d10f5e94956242d535
Yuzunoha <studysteadystandby@gmail.com>
2023-01-22 22:06:49 +0900

    整理してみた

--- app/index.js
+++ app/index.js
@@ -1,15 +1,22 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer(async (request, response) => {
-  const data = await new Promise((resolve) => {
-    fs.readFile('./html_files/index.html', 'UTF-8', (e, d) => resolve(d));
+const promiseReadFile = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'UTF-8', (err, data) => {
+      err ? reject(err) : resolve(data);
+    });
   });
+};
+
+const getFromClient = async (request, response) => {
+  const filePath = './html_files/index.html';
+  const data = await promiseReadFile(filePath);
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(data);
   response.end();
-});
+};
 
+const server = http.createServer(getFromClient);
 server.listen(3000);
-
 console.log('起動!!!');
