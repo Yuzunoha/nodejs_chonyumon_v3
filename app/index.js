ce7d01aa394744e710d88c33fef2de6465678948
Yuzunoha <studysteadystandby@gmail.com>
2023-01-22 21:42:47 +0900

    h1になった

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World\n');
+  // response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('<html><h1>Hello World</h1></html>');
 });
 
 server.listen(3000);
